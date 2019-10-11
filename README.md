7bb4b99401c55738d68659782071f5d716ea818c
aliaasayed <aliaasayed45@gmail.com>
2019-10-11 21:16:31 +0200

    handle request timeout

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # server-availability-using-nodejs
+simple node module to check if a list of servers is on
+
+## How to Run?
+
+1- clone project:
+```bash
+       https://github.com/aliaasayed/server-availability-using-nodejs.git
+```
+
+2- run app: 
+
+    # first install dependencies using "npm i"
+    # then start the server using "npm start"
+
+3- send get request to http://localhost:3000/find/server
+
+4- run unit test by running this command
+```bash
+    npm test
+```
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const server = express();
+var timeout = require('connect-timeout');
 
 server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -7,6 +8,14 @@ server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   next();
 });
+
+server.use(timeout(5000));
+server.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout)
+    next();
+}
 
 const serverRouter = require('./controllers/serverController')
 server.use("/", serverRouter);
