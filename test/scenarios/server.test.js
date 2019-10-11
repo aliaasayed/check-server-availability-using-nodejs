39bb6bc966fb6b4994e99096b73bfe814620dbcf
aliaasayed <aliaasayed45@gmail.com>
2019-10-11 23:03:50 +0200

    test

---
+++ test/scenarios/server.test.js
@@ -0,0 +1,20 @@
+const app = require('../../app');
+const expect = require('expect');
+const request = require("supertest");
+
+describe("Check avilability of list of servers", function () {
+    it("send request to all servers using find/server", function (done) {
+        request(app)
+            .get('/find/server')
+            .expect(function (res) {
+                expect(res.body.statusCode).toBe(200);
+                expect(res.body.data.url).toBeDefined();
+                expect(res.body.data.priority).toBeDefined();
+            })
+            .end(function (err, res) {
+                if (err)
+                    return done(err);
+                done();
+            });
+    });
+});
