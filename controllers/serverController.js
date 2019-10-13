d8c5bdbc63e930c31026184a1926d61a84e288f8
aliaasayed <aliaasayed45@gmail.com>
2019-10-13 17:54:21 +0200

    response status

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -5,8 +5,8 @@ const serverService = require("../services/serverService")
 //find servers
 router.get("/find/server", function (req, res) {
     serverService.findServer().then(data => {
-        res.json({ statusCode: 200, data: data });
-    }).catch(err => res.json({ statusCode: 400, data: err }));
+        res.status(200).json({ data: data });
+    }).catch(err => res.status(404).json({ err: err }));
 });
 
 module.exports = router;
